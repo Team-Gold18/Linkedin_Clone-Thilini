999bb83aa1ee925591ee5e649516993b2d9d9a62
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2022-02-13 21:53:37 -0800

    home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
+
+
 
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-            <Login/>
+            <Login />
+          </Route>
+          <Route path="/home">
+           <Header/>
+            <Home />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Rightside.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Rightside() {
+  return (<Container></Container>);
+}
+
+export default Rightside
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
